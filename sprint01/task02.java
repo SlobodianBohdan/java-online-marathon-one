17b422dd93cc8b123d1faac4a2547a27a4db2220
SlobodianBohdan <62740608+SlobodianBohdan@users.noreply.github.com>
2020-03-30 15:34:30 +0300

    Update task02.java

--- sprint01/task02.java
+++ sprint01/task02.java
@@ -1,7 +1,4 @@
-    public static void main(String[] args) {
-        System.out.println(doubleNumber(21));
 
-    }
     public static float doubleNumber(double number) {
         return (float) number * 2;
     }
