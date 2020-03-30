a1fe30215985f3a376687c484e47c1b72f76e65f
SlobodianBohdan <62740608+SlobodianBohdan@users.noreply.github.com>
2020-03-30 15:33:44 +0300

    Update task03.java

--- sprint01/task03.java
+++ sprint01/task03.java
@@ -1,1 +1,10 @@
-
+public static int century(int year) {
+    
+         if (year <= 100)
+            return 1;
+        else if (year % 100 == 0)
+            return year / 100;
+        else
+            return year / 100 + 1;
+    
+}
