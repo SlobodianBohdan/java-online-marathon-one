61ace1842e777579643bd5d41059fe3e68267e2a
SlobodianBohdan <62740608+SlobodianBohdan@users.noreply.github.com>
2020-03-30 15:29:28 +0300

    Update and rename task01.java to task02.java

---
+++ sprint01/task02.java
@@ -0,0 +1,7 @@
+    public static void main(String[] args) {
+        System.out.println(doubleNumber(21));
+
+    }
+    public static float doubleNumber(double number) {
+        return (float) number * 2;
+    }
