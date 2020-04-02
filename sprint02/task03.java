e399b52233e7ec1925019ad6f1c248dfbb1b2bec
SlobodianBohdan <62740608+SlobodianBohdan@users.noreply.github.com>
2020-04-02 12:57:34 +0300

    Update task03.java

--- sprint02/task03.java
+++ sprint02/task03.java
@@ -1,1 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+      
+          System.out.println(Solution.divide(10,3));
+          System.out.println(Solution.divide(7,-3));
 
+    }
+}
+
+class Solution {
+    static int divide(int dividend, int divisor) {
+        int sign = ((dividend < 0) ^
+                (divisor < 0)) ? -1 : 1;
+
+        dividend = Math.abs(dividend);
+        divisor = Math.abs(divisor);
+
+        int quotient = 0;
+
+        while (dividend >= divisor) {
+            dividend -= divisor;
+            ++quotient;
+        }
+        return sign * quotient;
+    }
+}
