8d843f6f5bc503b5b1bb0a847915eab95bec7a28
SlobodianBohdan <62740608+SlobodianBohdan@users.noreply.github.com>
2020-04-02 12:33:17 +0300

    Update task01.java

--- sprint02/task01.java
+++ sprint02/task01.java
@@ -1,1 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Solution.isPalindrome(121));
+    }
+}
 
+class Solution {
+    public static boolean isPalindrome(int x) {
+        if (x % 10 == 0) {
+            return x == 0;
+        }
+        int reverse = 0;
+        while (reverse < x) {
+            reverse = reverse * 10 + x % 10;
+            x /= 10;
+        }
+        return reverse == x || reverse / 10 == x;
+    }
+}
