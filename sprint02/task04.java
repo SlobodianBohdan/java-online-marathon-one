b90e52fbb4877b1de0249d5e194bb8a62ef4e82e
SlobodianBohdan <62740608+SlobodianBohdan@users.noreply.github.com>
2020-04-02 16:25:21 +0300

    Update task04.java

--- sprint02/task04.java
+++ sprint02/task04.java
@@ -1,1 +1,15 @@
 
+class Solution {
+    public static int mySqrt(int x) {
+        if (x == 0 || x == 1)
+            return x;
+
+        int result = 1, i = 1;
+
+        while (i <= x) {
+            result++;
+            i =  result * result;
+        }
+        return result - 1;
+    }
+}
