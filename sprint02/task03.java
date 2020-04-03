49af55c075e09a6ebe22138cb761a9d7785a19c4
SlobodianBohdan <62740608+SlobodianBohdan@users.noreply.github.com>
2020-04-03 17:10:30 +0300

    Update task03.java

--- sprint02/task03.java
+++ sprint02/task03.java
@@ -1,23 +1,27 @@
 
 class Solution {
     static int divide(int dividend, int divisor) {
-        int sign = ((dividend < 0) ^
-                (divisor < 0)) ? -1 : 1;
-
-        if (dividend < 0){
-            dividend *= -1;
-        }
-
-        if (divisor < 0){
-            divisor *= -1;
-        }
-
+        int sign = ((dividend < 0) ^ (divisor < 0)) ? -1 : 1;
         int quotient = 0;
 
+        if (dividend < 0) {
+            dividend *= -1;
+        }
+        if (divisor < 0) {
+            divisor *= -1;
+        }
+        
         while (dividend >= divisor) {
             dividend -= divisor;
             ++quotient;
         }
-        return sign * quotient;
+
+        int result = sign * quotient;
+        int remainder = result % 10;
+        if (result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE / 10 && remainder > 7))
+            return Integer.MAX_VALUE;
+        if (result < Integer.MIN_VALUE / 10 || (result == Integer.MIN_VALUE / 10 && remainder < -8))
+            return Integer.MIN_VALUE;
+        return result;
     }
 }
