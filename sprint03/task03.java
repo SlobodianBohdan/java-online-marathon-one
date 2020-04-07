f3d6ce92ae82933d918884f6a04d726023cacdc9
SlobodianBohdan <62740608+SlobodianBohdan@users.noreply.github.com>
2020-04-07 22:17:03 +0300

    Update task03.java

--- sprint03/task03.java
+++ sprint03/task03.java
@@ -1,1 +1,14 @@
+// Create a listMapCompare() method of the MyUtils class to compare the contents of a list of strings and the values of a map. 
+// For example, for a given list [aa, bb, aa, cc] and map {1=cc, 2=bb, 3=cc, 4=aa, 5=cc} you should get true.
 
+import java.util.Set;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+public class MyUtils {
+    // Code
+    public boolean listMapCompare(List<String> list, Map<String, String> map) {
+        // Code
+         return new HashSet<>(list).containsAll(map.values());
+    }
+}
