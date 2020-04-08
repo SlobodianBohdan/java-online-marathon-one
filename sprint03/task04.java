c12207cb3701aa3ba56e3e80ae3f8ee8135aefbc
SlobodianBohdan <62740608+SlobodianBohdan@users.noreply.github.com>
2020-04-08 23:01:36 +0300

    Update task04.java

--- sprint03/task04.java
+++ sprint03/task04.java
@@ -1,1 +1,26 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
+public class MyUtils {
+    // Code
+    public Map<String, List<String>> createNotebook(Map<String, String> phones) {
+        // Code
+        Map<String, List<String>> map = new HashMap<>();
+        if (phones != null) {
+            Set<String> set = new HashSet<>(phones.values());
+            set.forEach(i -> {
+                List<String> list = phones.entrySet()
+                        .stream()
+                        .filter(e ->(Objects.nonNull(e.getValue()) && e.getValue().equals(i)) || e.getValue() == i)
+                        .map(Map.Entry::getKey)
+                        .collect(Collectors.toList());
+                map.put(i, list);
+            });
 
+        }
+        return map;
+    }
+}
