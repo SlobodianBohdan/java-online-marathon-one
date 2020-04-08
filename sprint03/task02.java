553cd98f793f79ebd57d9f659be478f8de9d3ddb
SlobodianBohdan <62740608+SlobodianBohdan@users.noreply.github.com>
2020-04-08 15:51:08 +0300

    Update task02.java

--- sprint03/task02.java
+++ sprint03/task02.java
@@ -1,1 +1,25 @@
-
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+public class MyUtils {
+    // Code
+    public List<String> strSort(List<String> originList) {
+        // Code
+        List<String> newList = new ArrayList<>();
+        originList.forEach(i->{
+            newList.add(i);
+        });
+        
+        Collections.sort(newList, (o1, o2) -> {
+            if(o1.length()<o2.length()){
+                return -1;
+            }if(o1.length()>o2.length()){
+                return 1;
+            }else{
+                return o1.compareTo(o2);
+            }
+        });
+        return newList;
+    }
+}
