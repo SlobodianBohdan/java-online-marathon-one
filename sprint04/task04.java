5b2b65172ef7422c9306c42707d6af9ac347cfbb
SlobodianBohdan <62740608+SlobodianBohdan@users.noreply.github.com>
2020-04-10 12:30:55 +0300

    Update task04.java

--- sprint04/task04.java
+++ sprint04/task04.java
@@ -1,3 +1,11 @@
+// Suppose we have the next class:
+// Create a new version of the Circle class where the draw method will be overloaded three times:
+// Using the one parameter color of String type.
+// Using the one parameter scale of float type.
+// Using two parameters color and scale of String and float type.
+    
+// * For correct passing of all tests don't use print and println methods in your code.
+
 class Circle {
     private double radius;
 
