921aacc634d935d847b85879bbdb2a0c18317de1
SlobodianBohdan <62740608+SlobodianBohdan@users.noreply.github.com>
2020-04-13 16:40:41 +0300

    Update task02.java

--- sprint05/task02.java
+++ sprint05/task02.java
@@ -1,12 +1,12 @@
-•Implement class User with protected fields name, 
-surname and public method getFullName() which returns name and surname with one space between them. 
-Constructor of User define as public with two parameters – for initialization name and surname
-•Implement also class Student that is inherited from User. 
-This class must have integer protected field year – the year of entering university,
-and public method getCourseNumber() – evaluates year of studying based on year property and current year: 
-the difference between years + 1. (If the value is not from the range from 1 to 6, the metod should return -1). 
-Conctructor of Student should be public and have 3 parameters: year, name and surname and pass name and surname to the base class. 
-(year parameter should be the first.)
+// •Implement class User with protected fields name, 
+// surname and public method getFullName() which returns name and surname with one space between them. 
+// Constructor of User define as public with two parameters – for initialization name and surname
+// •Implement also class Student that is inherited from User. 
+// This class must have integer protected field year – the year of entering university,
+// and public method getCourseNumber() – evaluates year of studying based on year property and current year: 
+// the difference between years + 1. (If the value is not from the range from 1 to 6, the metod should return -1). 
+// Conctructor of Student should be public and have 3 parameters: year, name and surname and pass name and surname to the base class. 
+// (year parameter should be the first.)
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
