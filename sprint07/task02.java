167c83a9328945c1008e24594a60c30b9b80c23c
SlobodianBohdan <62740608+SlobodianBohdan@users.noreply.github.com>
2020-04-20 20:50:09 +0300

    Update task02.java

--- sprint07/task02.java
+++ sprint07/task02.java
@@ -1,1 +1,33 @@
+// Suppose we have the next class:
 
+// NameList class
+// Create public inner class named Iterator inside NameList class that correspond the next class diagram:
+
+// * For correct passing of all tests don't use print and println methods in your code.
+
+
+class NameList {
+    public class Iterator{
+        private int counter;
+
+        private Iterator(){
+
+        }
+
+        public boolean hasNext (){
+            return counter < names.length? true : false;
+        }
+
+        public String next(){
+            String value = names[counter];
+            counter++;
+            return value;
+        }
+
+    }
+    private String[] names = {"Mike", "Emily", "Nick", "Patric", "Sara"};
+
+    public Iterator getIterator() {
+        return new Iterator();
+    }
+}
