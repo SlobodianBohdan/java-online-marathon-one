c2443982d785e69545677448965e07c928953056
SlobodianBohdan <62740608+SlobodianBohdan@users.noreply.github.com>
2020-04-20 20:51:41 +0300

    Update task04.java

--- sprint07/task04.java
+++ sprint07/task04.java
@@ -1,1 +1,17 @@
+// Create a LineType enum that contains different kind of lines as constants SOLID, DOTTED, DASHED and DOUBLE.
+// Write a method that takes the constant of LineType type as input, and returns the message “The line is <LineType> type”.
+// Where <LineType>  is name of type wrote in lower case. For example: "The line is dotted type".
 
+// * For correct passing of all tests don't use print and println methods in your code.
+
+
+enum LineType {
+    SOLID, DOTTED, DASHED, DOUBLE;
+}
+static String LineType(LineType lineType) {
+    return "The line is " + lineType.toString().toLowerCase() + " type";
+}
+
+public static String drawLine(LineType lineType) {
+        return LineType(lineType);
+}
