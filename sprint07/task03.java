02ece8e84cf81182c52416e0f8aca2dd0da222a6
SlobodianBohdan <62740608+SlobodianBohdan@users.noreply.github.com>
2020-04-20 20:50:59 +0300

    Update task03.java

--- sprint07/task03.java
+++ sprint07/task03.java
@@ -1,1 +1,47 @@
+// Suppose we have execute method with next signature:
+// execute method
+// where the Strategy is the next interface :
+// Strategy interface
+// Using anonymous classes concept, call the execute method 4 times with different strategy (override method doOperation from Strategy interface):
+// Add a to b   \\   (a + b)
+// Subtract b from a   \\   (a - b)
+// Multiply a by b   \\   (a * b)
+// Divide a by b   \\   (a / b)
+// * For correct passing of all tests don't use print and println methods in your code.
 
+
+public static void addAtoB(int a, int b) {
+    execute( a, b, new Strategy(){
+        @Override
+        public double doOperation(int a, int b) {
+            return a + b;
+        }
+    });
+}
+
+public static void subtractBfromA(int a, int b) {
+    execute( a, b, new Strategy(){
+        @Override
+        public double doOperation(int a, int b) {
+            return a - b;
+        }
+    });
+}
+
+public static void multiplyAbyB(int a, int b) {
+    execute( a, b, new Strategy(){
+        @Override
+        public double doOperation(int a, int b) {
+            return a * b;
+        }
+    });
+}
+
+public static void divideAbyB(int a, int b) {
+    execute( a, b, new Strategy(){
+        @Override
+        public double doOperation(int a, int b) {
+            return a / b;
+        }
+    });
+}
