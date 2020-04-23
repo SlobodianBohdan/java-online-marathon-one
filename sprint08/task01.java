5950e98e788bc8a48fd8f5e788ef919d27495538
SlobodianBohdan <62740608+SlobodianBohdan@users.noreply.github.com>
2020-04-23 17:22:11 +0300

    Update task01.java

--- sprint08/task01.java
+++ sprint08/task01.java
@@ -1,1 +1,16 @@
+// Please, implement a static method getCount that takes an array of integers as the first parameter. The second parameter - a functional interface that works with integers and defines a condition. 
+// The method should return the count of elements in the array that satisfy the condition defined by the second argument.
 
+
+import java.util.Arrays;
+import java.util.function.Predicate;
+class MyUtils {
+
+    public static int getCount(int [] a, Predicate<Integer> predicate){
+        int count = 0;
+        for (int i : a) {
+            count = predicate.test(i) ? ++count : count;
+        }
+       return count;
+    }
+}
