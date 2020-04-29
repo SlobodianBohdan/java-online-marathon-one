4101bb171c3d9be0116cd1cc325dfa38d1a9257b
SlobodianBohdan <62740608+SlobodianBohdan@users.noreply.github.com>
2020-04-29 23:24:40 +0300

    Update task02.java

--- sprint09/task02.java
+++ sprint09/task02.java
@@ -1,1 +1,42 @@
+// Given a text containing brackets  '(', ')', '{', '}', '[' and ']'.
+// Sequences "\\(", "\\)", "\\[", "\\]", "\\{" and "\\}" are not brackets.
+// Write a boolean verifyBrackets(String text) method of the MyUtils class to check a input text.
+// The brackets must close in the correct order, for example "()", "()[]{}", "{(())}" and "(\\()" are all valid but "(]", ")(" and "([)]" are not.
 
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ArrayDeque;
+
+class MyUtils{
+    public boolean verifyBrackets(String text) {
+        ArrayDeque<Character> st = new ArrayDeque<Character>();
+        int count = 0;
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) == '\\'){
+                count++;
+                continue;
+            }
+            if (count ==1){
+                count++;
+                if (count > 1){
+                    count = 0;
+                }
+                continue;
+            }
+            if ((text.charAt(i) == '(') || (text.charAt(i) == '[') ||
+                    (text.charAt(i) == '{')) {
+                st.push(text.charAt(i));
+            }
+            else {
+                if (st.isEmpty()) return false;
+                if ((text.charAt(i) == ')') && (st.peek() != '(')) return false;
+                if ((text.charAt(i) == ')') && (st.peek() != '(')) return false;
+                if ((text.charAt(i) == ']') && (st.peek() != '[')) return false;
+                if ((text.charAt(i) == '}') && (st.peek() != '{')) return false;
+                st.pop();
+            }
+        }
+        return st.isEmpty();
+    }
+}
