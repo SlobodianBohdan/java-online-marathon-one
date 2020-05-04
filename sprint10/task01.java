7fdcf42305cc7ae7812004904ce9f9536be3af30
SlobodianBohdan <62740608+SlobodianBohdan@users.noreply.github.com>
2020-05-04 09:33:42 +0300

    Update task01.java

--- sprint10/task01.java
+++ sprint10/task01.java
@@ -1,1 +1,19 @@
+//        Create a Wrapper generic class with generic private field of type T named value.
+//        Also in the Wrapper class define a parametrized public constructor and access methods with setValue and getValue name.
+//        * For correct passing of all tests don't use print and println methods in your code.
 
+class Wrapper1 <T>{
+    private T value ;
+
+    public Wrapper1(T value) {
+        this.value = value;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+}
