07c89667e20b87d9eb9e906b5312b944d83a415b
SlobodianBohdan <62740608+SlobodianBohdan@users.noreply.github.com>
2020-05-04 09:34:38 +0300

    Update task02.java

--- sprint10/task02.java
+++ sprint10/task02.java
@@ -1,1 +1,11 @@
+//        Suppose we have class hierarchy that corresponds next class diagram:
+//        Shape interface
+//        Using Wrapper generic class from task #1, modify it so that it can only wrap objects that implement the Shape interface (Square, Circle, etc.).
+//        For example:
+//        * For correct passing of all tests don't use print and println methods in your code.
 
+class ArrayUtil {
+    public static <T> T setAndReturn(T[] array, T value, int index){
+          return array[index] = value;
+    }
+}
