5b87446ea85876a9ae4d0b9a1ec212d7b488a5cf
SlobodianBohdan <62740608+SlobodianBohdan@users.noreply.github.com>
2020-05-04 09:35:25 +0300

    Update task03.java

--- sprint10/task03.java
+++ sprint10/task03.java
@@ -1,1 +1,21 @@
+//        Suppose we have class hierarchy that corresponds next class diagram:
+//        Shape interface
+//        Using Wrapper generic class from task #1, modify it so that it can only wrap objects that implement the Shape interface (Square, Circle, etc.).
+//        For example:
+//        * For correct passing of all tests don't use print and println methods in your code.
 
+class Wrapper <T extends Shape> {
+    private T value ;
+
+    public Wrapper(T value) {
+        this.value = value;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+}
