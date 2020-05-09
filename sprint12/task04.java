21a694cb8ad0460b37ec8a3682f0e4ac84bc99ba
SlobodianBohdan <62740608+SlobodianBohdan@users.noreply.github.com>
2020-05-09 23:36:42 +0300

    Update task04.java

--- sprint12/task04.java
+++ sprint12/task04.java
@@ -27,4 +27,54 @@
 // Thread1 success!
 // Please, don't change actions that change variables or print output within run methods. Use only thread synchronization assets.
 
+class MyThreads {
+    public final static Object den = new Object();
+    public final static Object ada = new Object();
 
+    public static int n;
+    public static int m;
+
+    public static Thread t1 = new Thread() {
+        public void run() {
+            synchronized (den) {
+                for(int i = 0; i < 5; i++, n++)
+                    System.out.println("Thread1 n = " + n);
+            }
+            synchronized (ada) {
+                try{
+                    ada.wait(250);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+                for(int i = 0; i < 5; i++, m++)
+                    System.out.println("Thread1 m = " + m);
+                System.out.println( "Thread1 success!");
+            }
+        }
+    };
+
+    public static Thread t2 = new Thread() {
+        public void run() {
+            synchronized (ada) {
+                try{
+                    ada.wait(100);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+                for(int i = 0; i < 5; i++, m++)
+                    System.out.println("Thread2 m = " + m);
+            }
+
+            synchronized (den) {
+                try{
+                    den.wait(100);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+                for(int i = 0; i < 5; i++, n++)
+                    System.out.println("Thread2 n = " + n);
+                System.out.println("Thread2 success!");
+            }
+        }
+    };
+}
