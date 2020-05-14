650ed504c3b2ded0bd22cd5492d8e55f92c4a2ba
SlobodianBohdan <62740608+SlobodianBohdan@users.noreply.github.com>
2020-05-14 09:59:32 +0300

    Update task01.java

--- sprint13/task01.java
+++ sprint13/task01.java
@@ -1,1 +1,9 @@
+// Write a method to convert date and time, represented via Date object to object of Calendar type.
+// * For correct passing of all tests don't use print and println methods in your code.
 
+
+public static Calendar convertDateToCalendar(Date date) {
+    Calendar calendar = Calendar.getInstance();
+    calendar.setTime(date);
+    return calendar;
+}
