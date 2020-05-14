1b3483b17af0189a8c3dabe23d4a770cb13baefc
SlobodianBohdan <62740608+SlobodianBohdan@users.noreply.github.com>
2020-05-14 10:02:53 +0300

    Update task06.java

--- sprint13/task06.java
+++ sprint13/task06.java
@@ -1,1 +1,7 @@
+// Create the method writeFile(String filename, String text) that write the text to file as sequence of bytes in binary format.
+// For example, the text fragment
+// should be represented in the file as a sequence of 7-bit bytes without spaces between them:
+// If less than 7 bits are required to represent the character then add to binary sequence leading zeros '0'.
+// * For correct passing of all tests don't use print and println methods in your code.
+
 
