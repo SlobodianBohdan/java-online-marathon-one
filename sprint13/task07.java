1ab68a11b12f042d28340f4704f0ec8afccc4214
SlobodianBohdan <62740608+SlobodianBohdan@users.noreply.github.com>
2020-05-14 10:03:24 +0300

    Update task07.java

--- sprint13/task07.java
+++ sprint13/task07.java
@@ -1,1 +1,6 @@
+// Create the method readFile(String filename) that read from file a sequence of bytes in binary format from previous task and return ridable string.
+// For example, the sequence of 7-bit bytes
+// should be represented as text fragment:
+// * For correct passing of all tests don't use print and println methods in your code.
+
 
