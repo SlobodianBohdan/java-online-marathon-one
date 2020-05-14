6e24a7d2b6e5933c12c2cf68c19041d63d8dbd22
SlobodianBohdan <62740608+SlobodianBohdan@users.noreply.github.com>
2020-05-14 10:01:01 +0300

    Update task03.java

--- sprint13/task03.java
+++ sprint13/task03.java
@@ -1,1 +1,7 @@
+// Write a method to check if a year is a leap year or not, using for this the LocalDate class.
+// If a year is leap then method should return true, otherwise - false.
+// * For correct passing of all tests don't use print and println methods in your code.
 
+public static boolean isLeapYear(int year) {
+    return (year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0)) ? true : false;
+}
