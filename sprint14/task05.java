1bced9c165d4fb3582dcb49e432841ee939d6b86
SlobodianBohdan <62740608+SlobodianBohdan@users.noreply.github.com>
2020-05-20 09:01:04 +0300

    Update task05.java

--- sprint14/task05.java
+++ sprint14/task05.java
@@ -1,1 +1,5 @@
-
+Create a Stream<Integer> duplicateElements(Stream<Integer> stream) method of the MyUtils class to return a sorted stream of duplicated elements of the input stream.
+For example, for a given elements
+[3, 2, 1, 1, 12, 3, 8, 2, 4, 2]
+you should get
+[1, 2, 3]
