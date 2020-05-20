060d5332de08df8c63a8f6973dc15fd7f41cafd8
SlobodianBohdan <62740608+SlobodianBohdan@users.noreply.github.com>
2020-05-20 09:00:45 +0300

    Update task04.java

--- sprint14/task04.java
+++ sprint14/task04.java
@@ -1,1 +1,5 @@
-
+// Create a int countNumbers(IntStream intNum, Stream<String> strNum) method of the MyUtils class to count of numbers that is divisible by 3 or contains the digit 3.
+// The parameters of the method are two Streams with integers and Strings with one number.
+// For example, for a given
+// [[3, 2, 1, 13, 21, 15], ["9", "4", "23", "0", "32", "5"]]
+// you should get 7
