5f6fa78deed9466299b8decd22d03c88f90183ca
SlobodianBohdan <62740608+SlobodianBohdan@users.noreply.github.com>
2020-05-20 09:00:19 +0300

    Update task03.java

--- sprint14/task03.java
+++ sprint14/task03.java
@@ -1,1 +1,7 @@
+// Create a int sumEven(Stream<IntStream> stream) method of the MyUtils class to sum minimal positive even numbers from all Streams.
+// Return zero if minimum positive even element was not found in stream.
+// For example, for a given
+// [[-2, -4, 1, 8, 3, 10], [2, -4, 4, 0, 3, 1], [1, -4, 3, 5, 3, 1]]
+// you should get 10 (8+2+0)
+
 
